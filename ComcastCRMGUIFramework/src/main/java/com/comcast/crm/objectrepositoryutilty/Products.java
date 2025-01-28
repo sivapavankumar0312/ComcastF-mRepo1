21f3edec7d527b706b259cfe96ae2cabdf2201c1
home <home@DESKTOP-LBNV72U>
2025-01-28 12:31:30 +0530

    product,Added in POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutilty/Products.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepositoryutilty;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+@FindBy(xpath="//img[@alt='Create Product...']")
+private WebElement createProImg;
+}
